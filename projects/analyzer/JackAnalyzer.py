09d39ea46ecd791762b53967453c11363a93912e
Frederic Mercille <fmercille@gmail.com>
2022-02-15 18:16:02 -0500

    The analyzer now returns the parse tree in XML format for the next step to process

--- projects/analyzer/JackAnalyzer.py
+++ projects/analyzer/JackAnalyzer.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+from xml.etree import ElementTree
+
 from JackException import JackException, FileNotFoundException
 from JackTokenizer import JackTokenizer
 from JackParser import JackParser
@@ -17,29 +19,36 @@ class JackAnalyzer:
     else:
       raise FileNotFoundException(file_or_dir)
 
-  def parse(self):
+  def parse(self, verbose=True):
+    _ret = {}
+
     for file in self._files:
       tokenizer = JackTokenizer(file)
       compiler = JackParser(tokenizer)
       output = compiler.compileClass()
 
-      # Write the token file
-      output_file = file[:-5] + 'T.xml' # Replace the .jack file extension by T.xml
-      with open(output_file, 'w') as f:
-        f.write("<tokens>" + '\r\n')
-        for token in tokenizer.xml():
-          f.write(token.strip() + '\r\n')
-        f.write("</tokens>" + '\r\n')
+      if verbose:
+        # Write the token file
+        output_file = file[:-5] + 'T.xml' # Replace the .jack file extension by T.xml
+        with open(output_file, 'w') as f:
+          f.write("<tokens>" + '\r\n')
+          for token in tokenizer.xml():
+            f.write(token.strip() + '\r\n')
+          f.write("</tokens>" + '\r\n')
 
-      # Write the parse tree
-      output_file = file[:-5] + '.xml' # Replace the .jack file extension by .xml
-      with open(output_file, 'w') as f:
-        for line in output:
-          f.write(line + '\r\n')
+        # Write the parse tree
+        output_file = file[:-5] + '.xml' # Replace the .jack file extension by .xml
+        with open(output_file, 'w') as f:
+          for line in output:
+            f.write(line + '\r\n')
+
+      _ret[file[:-5]] = ElementTree.fromstringlist(output)
+
+    return _ret
 
 if __name__ == '__main__':
   argparser = argparse.ArgumentParser(description="Jack Analyzer for the HACK machine")
   argparser.add_argument('input', help='Either a single .jack file, or a directory containing one or more .jack files')
   args = argparser.parse_args()
 
-  JackAnalyzer(args.input).parse()
+  JackAnalyzer(args.input).parse(verbose=False)
